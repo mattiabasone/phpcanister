714952ed0b76be991e6ef336e4e3af0ee1348548
Mattia Basone <mattia.basone@gmail.com>
2019-01-27 11:51:44 +0100

    Added container name Added HTTPS support Added XDebug IDE Key customization Added sudo and changed default user to phpcanister

--- nginx-php/startup.sh
+++ nginx-php/startup.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
 # Adding host machine hostname
-echo -e "\n# Hostname for gateway" >> /etc/hosts
-echo -e "`/sbin/ip route|awk '/default/ { print $3 }'`\tdocker.host.internal\n" >> /etc/hosts
+sudo bash -c 'echo -e "\n# Hostname for gateway" >> /etc/hosts'
+sudo bash -c "echo -e \"`/sbin/ip route|awk '/default/ { print $3 }'`\tdocker.host.internal\n\" >> /etc/hosts"
 
-/usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
+TMP_PHP_XDEBUG_IDE_KEY=${PHP_XDEBUG_IDE_KEY}
+for tmp_php_version in 5.6 7.0 7.1 7.2 7.3;
+    do
+        sudo bash -c "sed -i -r \"s|xdebug.idekey=([a-zA-Z0-9_]+)|xdebug.idekey=${TMP_PHP_XDEBUG_IDE_KEY}|g\" /etc/php/${tmp_php_version}/fpm/conf.d/20-xdebug.ini"
+done
+
+sudo /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
